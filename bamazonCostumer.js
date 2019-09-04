5577421115d463977c1970e294b93cc9a2f4921a
Polinet <uchpolinet@gmail.com>
2019-09-04 23:43:35 +1000

    created a function to proceed the order

--- bamazonCostumer.js
+++ bamazonCostumer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var table = require("console.table");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -15,32 +16,58 @@ connection.connect(function(error) {
   askCostumer();
 });
 
-var askCostumer = function() {
-  inquirer.prompt([
-    {
-      name: "productID",
-      type: "input",
-      message: "Enter product ID you'd like to purchase: ",
-      validate: function(value) {
-        if (isNaN(value) && value < 11) {
-          return true;
-        } else {
-          return false;
+function askCostumer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "ID",
+        message: "Enter product ID you'd like to purchase:\n",
+        validate: function(value) {
+          if (!isNaN(value) && value < 11) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      },
+
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many would you like to buy?\n",
+        validate: function(value) {
+          if (!isNaN(value)) {
+            return true;
+          } else {
+            return false;
+          }
         }
       }
-    },
+    ])
+    .then(function(answer) {
+      var selectedItemId = answer.ID;
+      console.log("Your purchase item ID: " + selectedItemId);
 
-    {
-      name: "quantity",
-      type: "input",
-      message: "How many would you like to buy?",
-      validate: function(value) {
-        if (isNaN(value)) {
-          return true;
-        } else {
-          return false;
-        }
+      var chosenQuantity = answer.quantity;
+      console.log("Your purchase quantity: " + chosenQuantity);
+
+      console.log(answer);
+      proceedOrder(selectedItemId, chosenQuantity);
+    });
+}
+
+function proceedOrder(userSelectedID, userChosenQuantity) {
+  connection.query(
+    "SELECT * FROM products WHERE id= " + userSelectedID,
+    function(resp, err) {
+      if (err) throw err;
+      if (userChosenQuantity <= resp[0].stock_quantity) {
+        console.log(userChosenQuantity);
+        console.log(resp[0].stock_quantity);
+      } else {
+        console.log("insufficient amount");
       }
     }
-  ]);
-};
+  );
+}
