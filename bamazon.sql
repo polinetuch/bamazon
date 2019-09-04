bbf1da0f24ee3c4a5e13c1a2f8570e3d91973c5f
Polinet <uchpolinet@gmail.com>
2019-09-04 21:16:30 +1000

    import data from csv file to database

--- bamazon.sql
+++ bamazon.sql
@@ -1,21 +1,14 @@
-DROP DATABASE IF EXISTS bamazon_DB;
+DROP DATABASE IF EXISTS bamazon;
 
-CREATE DATABASE bamazon_DB;
+CREATE DATABASE bamazon;
 
-USE bamazon_DB;
+USE bamazon;
 
 CREATE TABLE products (
 id INT NOT NULL AUTO_INCREMENT,
-product_name VARCHAR (50),
+product_name VARCHAR(50),
 department_name VARCHAR(50),
 price INT NOT NULL,
 stock_quantity INT NOT NULL,
-PRIMARY KEY (id) 
+PRIMARY KEY(id)
 );
-
-SELECT * FROM `products`;	
-INSERT INTO products (product_name, department_name, price, stock_quantity) 
-VALUES ("Tank top", "Clothes", 15, 20),
-("High heels", "Footwear", 35, 40),
-("Ring", "Jewellery", 5, 30)
-
