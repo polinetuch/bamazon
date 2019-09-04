678f64b795eee25d37cdd181720de69a9dea4fd1
Polinet <uchpolinet@gmail.com>
2019-09-04 20:09:26 +1000

    installed npm package and files: mysql & inquirer and created a connection to the database from js file

--- bamazon.sql
+++ bamazon.sql
@@ -1,8 +1,8 @@
-DROP DATABASE IF EXISTS bamazon;
+DROP DATABASE IF EXISTS bamazon_DB;
 
-CREATE DATABASE bamazon;
+CREATE DATABASE bamazon_DB;
 
-USE bamazon;
+USE bamazon_DB;
 
 CREATE TABLE products (
 id INT NOT NULL AUTO_INCREMENT,
@@ -13,6 +13,9 @@ stock_quantity INT NOT NULL,
 PRIMARY KEY (id) 
 );
 
-SELECT * FROM `products`;
-INSERT INTO products (product_name, department_name, price, stock_quantity) VALUES ("Tank top", "Clothes", 15, 20)
+SELECT * FROM `products`;	
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ("Tank top", "Clothes", 15, 20),
+("High heels", "Footwear", 35, 40),
+("Ring", "Jewellery", 5, 30)
 
---
+++ bamazonCostumer.js
@@ -0,0 +1,14 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "bamazon_DB"
+});
+
+connection.connect(function(error) {
+  if (error) throw error;
+});
